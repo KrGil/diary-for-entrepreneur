d7221c9c3915947fbbb02bd2daf5f22194601183
KrGil <Gil@Changui-MacBook-Air.local>
2017-07-10 15:20:05 +0900

    first commit

---
+++ app/src/main/java/com/example/gil/diary/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.gil.diary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private EditText mEditText;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButton = (Button) findViewById(R.id.button);
+        mEditText= (EditText) findViewById(R.id.editText);
+
+        View.OnClickListener firstClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = mEditText.getText().toString();
+                mTextView.setText(text);
+                mEditText.setText("");
+            }
+        };
+        mButton.setOnClickListener(firstClickListener);
+
+    }
+}
